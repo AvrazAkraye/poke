c9645da852b872b9cd0e6ba1852a4ab38182abcf
akrayw <avrazavraz@avraz-mac.local>
2016-06-19 09:48:49 +0100

    Initial Commit

---
+++ pokimon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokimon
+//
+//  Created by akrayw on 6/19/16.
+//  Copyright © 2016 akraywcom.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
